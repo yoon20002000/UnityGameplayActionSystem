8d56fb0346e5b67251aae42a7ab25bb18d97c674
yoon20002000 <yoon20002000@naver.com>
2025-02-26 13:58:10 +0900

    Action 추가 완료. ActionSystem 현 Action에 필요한 작업만 완료.

---
+++ Assets/Scripts/Action.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+public class Action : MonoBehaviour
+{
+    public void Initialize(ActionSystem InActionSystem)
+    {
+        actionSystem = InActionSystem;
+    }
+
+    public ActionSystem GetActionSystem()
+    {
+        return actionSystem;
+    }
+
+    public GameplayTags GetActivationTag()
+    {
+        return activationTag;
+    }
+    public bool GetAtuoStart()
+    {
+        return bAutoStart;
+    }
+    protected void SetAtuoStart(bool bInAutoStart)
+    {
+        bAutoStart = bInAutoStart;
+    }
+    public bool IsRunning()
+    {
+        return bIsRunning;
+    }
+
+    public virtual bool IsCanStart(GameObject instigator)
+    {
+        if(IsRunning() == true)
+        {
+            return false;
+        }
+        if(actionSystem.ActiveTagHasAny(blockedTags))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public virtual void StartAction(GameObject inInstigator)
+    {
+        actionSystem.SetActiveTags(grantsTags);
+
+        bIsRunning = true;
+        instigator = inInstigator;
+
+        actionSystem.OnActionStated.Invoke(actionSystem, this);
+    }
+
+    public virtual void StopAction(GameObject inInstigator)
+    {
+        actionSystem.UnSetActiveTags(grantsTags);
+
+        bIsRunning = false;
+        instigator = inInstigator;
+
+        actionSystem.OnActionStoped.Invoke(actionSystem, this);
+    }
+
+    protected ActionSystem actionSystem;
+
+    // editor에서 하나만 선택하게 해야 됨.
+    [SerializeField]
+    protected GameplayTags activationTag;
+
+    [SerializeField]
+    protected GameplayTags grantsTags;
+    [SerializeField]
+    protected GameplayTags blockedTags;
+
+    protected bool bIsRunning = false;
+    protected GameObject instigator = null;
+
+    protected float timeTarted;
+
+    protected bool bAutoStart = false;
+}
---
+++ Assets/Scripts/ActionSystem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+public class ActionSystem : MonoBehaviour
+{
+    public delegate void OnActionStatedChanged(ActionSystem actionSystem, Action action);
+
+    public OnActionStatedChanged OnActionStated;
+    public OnActionStatedChanged OnActionStoped;
+
+    protected GameplayTags activeTags;
+
+    
+    public bool ActiveTagHasAny(GameplayTags checkTags)
+    {
+        return hasAny(activeTags, checkTags);
+    }
+    protected bool hasAny(GameplayTags targetTags, GameplayTags checkTags)
+    {
+        return (targetTags & checkTags) != GameplayTags.None_Action;
+    }
+
+    public void SetActiveTags(GameplayTags grantsTags)
+    {
+        setTags(activeTags, grantsTags);
+    }
+    public void UnSetActiveTags(GameplayTags grantsTags)
+    {
+        unsetTags(activeTags, grantsTags);
+    }
+    protected void setTags(GameplayTags targetTags, GameplayTags setTags)
+    {
+        targetTags |= setTags;
+    }
+    protected void unsetTags(GameplayTags targetTags, GameplayTags unsetTags)
+    {
+        targetTags &= ~unsetTags;
+    }
+}
