16e864c961f279e3c07968004a01893c82faaed0
yoon20002000 <yoon20002000@naver.com>
2025-02-26 13:56:41 +0900

    Gameplay Tags 를 Enum으로 정의

---
+++ Assets/Scripts/GameplayTags.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+[Flags]
+public enum GameplayTags
+{
+    None_Action                     = 0,
+    Action_Attack                   = 1 << 0,
+    Action_Deffence                 = 1 << 1,
+    Action_Dash                     = 1 << 2,
+    Action_Skill                    = 1 << 3,
+    Status_Attacking                = 1 << 4,
+    Status_Defencing                = 1 << 5,
+    Status_Stun                     = 1 << 6,
+    Status_Invincibility            = 1 << 7,
+}
