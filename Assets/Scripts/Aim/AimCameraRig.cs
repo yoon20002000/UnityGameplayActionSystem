f4a5c66829b14c292a2d42f9b29d5e42309d2a73
yoon20002000 <yoon20002000@naver.com>
2025-03-01 12:24:10 +0900

    CameraRig 및 Cursor 설정 추가

---
+++ Assets/Scripts/Aim/AimCameraRig.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class AimCameraRig : CinemachineCameraManagerBase, Unity.Cinemachine.IInputAxisOwner
+{
+    public InputAxis AimMode = InputAxis.DefaultMomentary;
+
+    [SerializeField]
+    private CinemachineVirtualCameraBase aimCamera;
+    [SerializeField]
+    private CinemachineVirtualCameraBase freeCamera;
+
+    bool IsAiming => AimMode.Value > 0.5f;
+    public void GetInputAxes(List<IInputAxisOwner.AxisDescriptor> axes)
+    {
+        axes.Add(new() { DrivenAxis = () =>ref AimMode, Name = "Aim" });
+    }
+
+    protected override CinemachineVirtualCameraBase ChooseCurrentCamera(Vector3 worldUp, float deltaTime)
+    {
+        CinemachineVirtualCameraBase oldCam = (CinemachineVirtualCameraBase)LiveChild;
+        CinemachineVirtualCameraBase newCam = IsAiming ? aimCamera : freeCamera;
+        if(oldCam != newCam)
+        {
+            // 바라보는거 세팅을 해줘야 됨.
+            Debug.Log("바라봐");
+        }
+        return newCam;
+    }
+}
---
+++ Assets/Scripts/CursorManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
+}
